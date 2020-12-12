5ac52402d83c88bc8b7385d2a6ddecf970ddc613
jkawaoka <jkawaoka@gmail.com>
2020-12-12 22:23:12 +0900

    プロジェクトのひな形作成

--- pil-test.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image, ImageDraw
-
-file_path = './output.png'
-
-image = Image.open(file_path)
-draw = ImageDraw.Draw(image)
-
-draw.text((0,0), 'TEST')
-image.save('output2.png', 'PNG')
